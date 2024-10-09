dfb123a64f33ec7e0bfdd7b8f78b727af9801a2f
NachoHerrero <116599852+NHerreroo@users.noreply.github.com>
2024-10-09 13:57:30 +0200

    Improving Player Animation right left and idle

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,32 +3,38 @@ extends CharacterBody3D
 var pauseMenu = preload("res://Scenes/pause_menu.tscn")
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const LERP_AMOUNT = 0.3  # Controla la suavidad del movimiento
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var current_animation = ""
+
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the movement.
 	var input_dir = Input.get_vector("Keyboard_A", "Keyboard_D", "Keyboard_W", "Keyboard_S")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	# Aplica lerp para suavizar el movimiento en los ejes X y Z
+	velocity.x = lerp(velocity.x, direction.x * SPEED, LERP_AMOUNT)
+	velocity.z = lerp(velocity.z, direction.z * SPEED, LERP_AMOUNT)
 
 	move_and_slide()
 	
-func _ready():
-	$Sprites/AnimationPlayer.play("idle")
+	
+	#me detecta la tecla de direccion y llama a la func que me hace la animación pasandole el nombre de la anim
+	if Input.is_action_pressed("Keyboard_D"):
+		play_animation("walk_right")
+	elif Input.is_action_pressed("Keyboard_A"):
+		play_animation("walk_left")
+	elif Input.is_action_pressed("Keyboard_W"):
+		play_animation("idle")
+	elif Input.is_action_pressed("Keyboard_S"):
+		play_animation("idle")
+	else:
+		play_animation("idle")
+
+# func para  la animación si no está ya en reproducción
+func play_animation(anim_name: String):
+	if current_animation != anim_name:
+		$Sprites/AnimationPlayer.play(anim_name)
+		current_animation = anim_name
