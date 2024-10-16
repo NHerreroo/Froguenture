de2b70abf3f9d80d9c2415d699292dacba3cc330
NachoHerrero <116599852+NHerreroo@users.noreply.github.com>
2024-10-16 13:53:24 +0200

    Better Map Icon

--- Scripts/MapGeneration/map.gd
+++ Scripts/MapGeneration/map.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name Map
 
 const SCROLL_SPEED := 35
+const SCROLL_SMOOTHNESS: float = 0.1
 const  MAP_ROOM = preload("res://Scenes/Map/map_room.tscn")
 const  MAP_LINE = preload("res://Scenes/Map/map_line.tscn")
 
@@ -29,21 +30,21 @@ func _input(event: InputEvent) -> void:
 		camera_2d.position.y += SCROLL_SPEED
 	
 	camera_2d.position.y = clamp(camera_2d.position.y, -camera_edge_y, 0)
-	
+
 func generate_new_map() -> void:
 	floors_climbed = 0
 	map_data = map_generator.generate_map()
 	create_map()
-	
+
 func create_map() -> void:
 	for current_floor in map_data:
 		for room in current_floor:
 			if room.next_rooms.size() > 0:
 				_spawn_room(room)
-				
+
 	var middle := floori(MapGenerator.MAP_WIDTH * 0.5)
 	_spawn_room(map_data[MapGenerator.FLOORS-1][middle])
-	
+
 	var map_width_pixels := MapGenerator.X_DIST * (MapGenerator.MAP_WIDTH - 1)
 	visuals.position.x = (get_viewport_rect().size.x - map_width_pixels) / 2
 	visuals.position.y = get_viewport_rect().size.y / 5  # Cambia este valor para empezar más arriba	
@@ -52,47 +53,47 @@ func unlock_floor(which_floor: int = floors_climbed) -> void:
 	for map_room in rooms.get_children():
 		if map_room.room.row == which_floor:
 			map_room.available = true
-			
+
 func unlock_next_room() -> void:
 	for map_room in rooms.get_children():
 		if last_room.next_rooms.has(map_room.room):
 			map_room.available = true
-			
+
 func show_map() -> void:
 	self.show()
 	$MapBackground.show()
 	camera_2d.enabled = true
-	
+
 func hide_map() -> void:
 	self.hide()
 	$MapBackground.hide()
 	camera_2d.enabled = false
-	
+
 func _spawn_room(room: Room) -> void:
 	var new_map_room := MAP_ROOM.instantiate() as MapRoom
 	rooms.add_child(new_map_room)
 	new_map_room.room = room
 	new_map_room.selected.connect(_on_map_room_selected)
 	_connect_lines(room)
-	
+
 	if room.selected and room.row < floors_climbed:
 		new_map_room.show_selected()
-		
+
 func _connect_lines(room: Room) -> void:
 	if room.next_rooms.is_empty():
 		return
-		
+
 	for next in room.next_rooms:
 		var new_map_line := MAP_LINE.instantiate() as Line2D
 		new_map_line.add_point(room.position)
 		new_map_line.add_point(next.position)
 		lines.add_child(new_map_line)
-		
+
 func _on_map_room_selected(room: Room) -> void:
 	for map_room in rooms.get_children():
 		if map_room.room.row == room.row:
 			map_room.available = false  # Bloquea las habitaciones del mismo piso
-	
+
 	last_room = room
 	floors_climbed += 1
 	emit_signal("map_exited", room)  # Emitimos la señal para notificar a la escena principal
--- Scripts/MapGeneration/map_room.gd
+++ Scripts/MapGeneration/map_room.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Button
 class_name MapRoom
 
 
@@ -6,11 +6,11 @@ signal selected(room:Room)
 
 const ICONS := {
 	Room.Type.NOT_ASSIGNED: [null, Vector2.ONE],
-	Room.Type.MONSTER: [preload("res://Sprites/icons/skull.png"), Vector2.ONE],
-	Room.Type.TREASURE: [preload("res://Sprites/icons/treasure.png"), Vector2.ONE],
-	Room.Type.CAMPFIRE: [preload("res://Sprites/icons/campfire.png"), Vector2(0.6, 0.6)],
-	Room.Type.SHOP: [preload("res://Sprites/icons/shoop.png"), Vector2(0.6, 0.6)],
-	Room.Type.BOSS: [preload("res://Sprites/icons/boss.png"), Vector2(1.25, 1.25)],
+	Room.Type.MONSTER: [preload("res://Sprites/icons/skull.png"), Vector2(0.4, 0.4)],
+	Room.Type.TREASURE: [preload("res://Sprites/icons/treasure.png"), Vector2(0.4, 0.4)],
+	Room.Type.CAMPFIRE: [preload("res://Sprites/icons/campfire.png"), Vector2(0.4, 0.4)],
+	Room.Type.SHOP: [preload("res://Sprites/icons/shoop.png"),Vector2(0.4, 0.4)],
+	Room.Type.BOSS: [preload("res://Sprites/icons/boss.png"), Vector2(0.8, 0.8)],
 }
 
 @onready var sprite_2d: Sprite2D = $Visuals/Sprite2D
@@ -20,9 +20,6 @@ const ICONS := {
 var available := false : set = set_available
 var room: Room : set = set_room
 
-func _ready():
-	$Button.grab_focus()
-	
 func set_available(new_value: bool) -> void:
 	available = new_value
 	
@@ -36,16 +33,15 @@ func set_room(new_data: Room) -> void:
 	position = room.position
 	sprite_2d.texture = ICONS[room.type][0]
 	sprite_2d.scale = ICONS[room.type][1]
-	
+
 func show_selected():
 	pass
 
 func on_map_room_selected() -> void:
 	selected.emit(room)
 
-
-func _on_button_pressed():
-	if not available or not $Button.pressed:
+func _on_pressed():
+	if not available or not pressed:
 		return
 
 	room.selected = true
