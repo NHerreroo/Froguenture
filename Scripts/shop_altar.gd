5ad0d378df0d42aee385415651037fa15d9a9449
NachoHerrero <116599852+NHerreroo@users.noreply.github.com>
2025-03-13 15:20:37 +0100

    shop system

--- Scripts/shop_altar.gd
+++ Scripts/shop_altar.gd
@@ -2,9 +2,27 @@ extends Node3D
 
 @onready var nine_patch_rect = $CanvasLayer/NinePatchRect
 
+
+var items = {
+	"PACK": {"price": 100, "description": "A useful pack.", "rarity": "common"},
+	"COLLECTOR": {"price": 200, "description": "Collects nearby items.", "rarity": "rare"},
+	"HEART": {"price": 50, "description": "Restores health.", "rarity": "common"},
+	"SHIELD": {"price": 150, "description": "Provides temporary shield.", "rarity": "rare"},
+	"SPEED": {"price": 120, "description": "Increases movement speed.", "rarity": "uncommon"},
+	"ATTACK": {"price": 180, "description": "Boosts attack power.", "rarity": "uncommon"},
+	"ATTACKSPEED": {"price": 170, "description": "Increases attack speed.", "rarity": "uncommon"},
+	"CRITICALDMG": {"price": 250, "description": "Increases critical damage.", "rarity": "rare"},
+	"CRITICALCHANCE": {"price": 230, "description": "Increases critical hit chance.", "rarity": "rare"}
+}
+
+
 func _ready() -> void:
 	$AnimationPlayer.play("float")
-	
+	var keys = items.keys()
+	var specific_key = keys[2]  # Índice 2 (tercer elemento)
+	var current_item = items[specific_key]
+	print(current_item.price)
+
 func _process(delta: float) -> void:
 	set_controller_text()
 
@@ -21,10 +39,10 @@ func animate_nine_patch_rect(show: bool) -> void:
 
 	if show:
 		nine_patch_rect.visible = true
-		tween.tween_property(nine_patch_rect, "position", Vector2(644.0, 693.0), 0.2)
+		tween.tween_property(nine_patch_rect, "position", Vector2(485.0, 796.0), 0.2)
 		tween.parallel().tween_property(nine_patch_rect, "modulate:a", 1.0, 0.2)
 	else:
-		tween.tween_property(nine_patch_rect, "position", Vector2(644.0, 693.0), 0.2)
+		tween.tween_property(nine_patch_rect, "position", Vector2(485.0, 796.0), 0.2)
 		tween.parallel().tween_property(nine_patch_rect, "modulate:a", 0.0, 0.2)
 		await tween.finished  # Esperar a que termine la animación antes de ocultarlo
 		nine_patch_rect.visible = false  # Ocultar después de la animación
@@ -35,3 +53,6 @@ func set_controller_text():
 		$CanvasLayer/NinePatchRect/Text.text = str("Press 'O' to buy")
 	else:
 		$CanvasLayer/NinePatchRect/Text.text = str("Press 'E' to buy")
+
+
+	
