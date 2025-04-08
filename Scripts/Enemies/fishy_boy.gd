e6bfb2926c4e66d1f7c09567e0729e8a10c3a955
NachoHerrero <116599852+NHerreroo@users.noreply.github.com>
2025-04-08 15:51:05 +0200

    coasas

--- Scripts/Enemies/fishy_boy.gd
+++ Scripts/Enemies/fishy_boy.gd
@@ -3,10 +3,16 @@ extends Enemy
 var bullet = preload("res://Scenes/Enemies/Misc/EnemyBullet.tscn")
 var currentState
 
+var walk = false
+
 func _ready() -> void:
+	await get_tree().create_timer(1).timeout
 	Global.enemies_remaining += 1
 	$AnimationPlayer.play("idle")
+	walk = true
+	
 	
 func _physics_process(delta: float) -> void:
-	super._physics_process(delta)
-	nav.target_position = player.global_transform.origin
+	if walk:
+		super._physics_process(delta)
+		nav.target_position = player.global_transform.origin
