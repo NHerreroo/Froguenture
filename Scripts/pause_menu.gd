2a3401abb569eced43023ff2b3405385a7ea83f7
NachoHerrero <116599852+NHerreroo@users.noreply.github.com>
2025-05-15 19:55:21 +0200

    Menu Pause God

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,21 +1,35 @@
 extends Control
 
-@onready var resumeButton = $PanelContainer/BoxContainer/Resume
+@onready var resumeButton = $ColorRect/Resume
 
+func _ready():
+	position.x = -800
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	
 func pause():
 	Global.is_game_paused = true
-	self.show()
 	get_tree().paused = true
-
+	
+	self.show()
+	
+	var tween = create_tween()
+	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tween.tween_property(self, "position:x", 0, 0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	
 func resume():
-	Global.card_focused = false #para evitar eprdier focus de la carta si pausas menu en item selection
-	Global.is_game_paused = false
-	self.hide()
-	get_tree().paused = false
-
+	Global.card_focused = false
+	
+	var tween = create_tween()
+	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tween.tween_property(self, "position:x", -800, 0.5).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_callback(func(): 
+		Global.is_game_paused = false
+		get_tree().paused = false
+		self.hide()
+	)
 
 func testEsc():
-	if Input.is_action_just_pressed("pause") and !get_tree().paused:
+	if Input.is_action_just_pressed("pause") and not get_tree().paused:
 		resumeButton.grab_focus()
 		pause()
 	elif Input.is_action_just_pressed("pause") and get_tree().paused:
@@ -27,12 +41,5 @@ func _on_resume_pressed():
 func _on_exit_pressed():
 	get_tree().quit()
 
-func _process(delta):
+func _process(_delta):
 	testEsc()
-
-
-func _on_check_box_toggled(toggled_on):
-	if toggled_on:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN);
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED);
